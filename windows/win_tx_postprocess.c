520dc78351f0c920431cc7428f6f93f7f773f26a
Dariusz Sosnowski <dariusz.sosnowski@codilime.com>
2018-10-05 18:34:34 +0200

    [WIP] windows: WinTxPostprocess
    Change-Id: Id75fe5453ade0480cd210ee68201aa7f69764b87

--- windows/win_tx_postprocess.c
+++ windows/win_tx_postprocess.c
@@ -1,1 +1,7 @@
 #include "win_tx_postprocess.h"
+
+PACKET_HANDLE
+WinTxPostprocess(struct vr_packet *VrPacket)
+{
+    return 0;
+}
--- windows/win_tx_postprocess.h
+++ windows/win_tx_postprocess.h
@@ -6,4 +6,14 @@
 #ifndef __WIN_TX_POSTPROCESS_H__
 #define __WIN_TX_POSTPROCESS_H__
 
+#include <basetsd.h>
+
+// Forward declaration.
+struct vr_packet;
+
+// Typedef for masking implementation specific handles.
+typedef UINT_PTR PACKET_HANDLE;
+
+PACKET_HANDLE WinTxPostprocess(struct vr_packet *VrPacket);
+
 #endif
